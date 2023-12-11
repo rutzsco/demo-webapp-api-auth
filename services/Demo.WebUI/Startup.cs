a80b17052fcb44b57ea37f8fb4fe012329c1859d
Scott Rutz <scrutz@microsoft.com>
2023-12-11 10:46:02 -0600

    Update Startup.cs

--- services/Demo.WebUI/Startup.cs
+++ services/Demo.WebUI/Startup.cs
@@ -28,8 +28,8 @@ namespace Demo.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
-                .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+            //services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+            //    .AddAzureAD(options => Configuration.Bind("AzureAd", options));
 
             services.AddControllersWithViews(options =>
             {
@@ -60,8 +60,8 @@ namespace Demo.WebUI
 
             app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
